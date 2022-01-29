8ad94052eaa42b7ad19ec7454b14bab780413b1f
NeoremFf <maksim.kedrovskiy@gmail.com>
2022-01-29 21:17:02 +0200

    Add Exception for Empty collection

---
+++ Neo/Collection/Exceptions/EmptyCollectionException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Neo.Collection.Exceptions
+{
+  public class EmptyCollectionException : Exception
+  {
+    public EmptyCollectionException(string collectionName) : base($"Collection is empty: {collectionName}.")
+    { }
+  }
+}
